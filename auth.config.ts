ed50555cea1b5992090495c7894fede441e6c719
ShaileshIshere <markvaldoz009@gmail.com>
2024-10-11 16:36:51 +0530

    fixed deployment issue

--- auth.config.ts
+++ auth.config.ts
@@ -3,7 +3,7 @@ import Credentials from "next-auth/providers/credentials"
 import { LoginSchema } from "@/schemas"
 import { getUserByEmail } from "@/data/user";
 import Google from "next-auth/providers/google";
-import Github from "next-auth/providers/Github";
+import Github from "next-auth/providers/github";
 import bcrypt from "bcryptjs";
  
 export default { 
