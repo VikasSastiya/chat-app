a72a49816eb05b48235c3a9a54c071f6cd7c4d5a
ShaileshIshere <markvaldoz009@gmail.com>
2024-11-28 20:31:16 +0530

    configured changes in next.config.mjs for initial state

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,10 @@ const nextConfig = {
         'platform-lookaside.fbsbx.com',
         'randomuser.me'
       ]
-    }
+    },
+    experimental: {
+      missingSuspenseWithCSRBailout: false,
+    },
   }
   
 export default nextConfig;
